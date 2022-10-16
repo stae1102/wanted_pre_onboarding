9ca8ee5d035cfaec67da5ce8b5beec78e796e58c
stae1102 <stae1102@gmail.com>
2022-10-17 00:16:44 +0900

    Remove: Remove tech service and model
    Because I decided that there don't need things about the tech, removed it.

--- src/api/tech/tech.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { TechService } from './tech.service';
-import { TechController } from './tech.controller';
-
-@Module({
-  controllers: [TechController],
-  providers: [TechService],
-})
-export class TechModule {}
