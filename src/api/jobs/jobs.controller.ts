828832893ced0f50c5d8e3d196b150c62b456eb1
stae1102 <stae1102@gmail.com>
2022-10-17 00:35:38 +0900

    Feat: Create announcement serivce
    New announcement service
    - find all announcements
    - search announcement by keyword
    - update announcement

--- src/api/jobs/jobs.controller.ts
+++ src/api/jobs/jobs.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateAnnouncementDto } from './dto/create-announcement.dto';
 import { JobsService } from './jobs.service';
 
@@ -9,5 +18,28 @@ export class JobsController {
   @Post()
   createAnnouncement(@Body() body: CreateAnnouncementDto) {
     return this.jobsService.createAnnouncement(body);
+  }
+
+  @Get()
+  getAllAnnouncement() {
+    return this.jobsService.getAllAnnouncement();
+  }
+
+  @Get('feat')
+  searchAnnouncementByKeyword(@Query('search') search) {
+    return this.jobsService.searchAnnouncementByKeyword(search);
+  }
+
+  @Get('/:companyId')
+  getAnnouncementById(@Param('companyId', ParseIntPipe) companyId) {
+    return this.jobsService.getAnnouncementById(companyId);
+  }
+
+  @Patch('/:jobId')
+  updateAnnouncement(
+    @Param('jobId', ParseIntPipe) jobId: number,
+    @Body() body: CreateAnnouncementDto,
+  ) {
+    return this.jobsService.updateAnnouncement(jobId, body);
   }
 }
