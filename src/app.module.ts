ebc089ffebbf252cc933e756d69015df009f3ed6
stae1102 <stae1102@gmail.com>
2022-10-16 13:50:29 +0900

    Style: Change line sequence to LF
    Change line sequence CRLF to LF
    - there were wrong settings about husky

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,9 +6,13 @@ import { AppService } from './app.service';
 import { LoggerMiddleware } from './middlewares/logger.middleware';
 import { PrismaModule } from './prisma/prisma.module';
 import { JobsModule } from './api/jobs/jobs.module';
+import { CompaniesModule } from './api/companies/companies.module';
+import { ApiModule } from './api/api.module';
+import { RouterModule } from '@nestjs/core';
+import { UsersModule } from './api/users/users.module';
 
 @Module({
-  imports: [PrismaModule, JobsModule],
+  imports: [PrismaModule, ApiModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
